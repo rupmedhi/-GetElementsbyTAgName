8de11f88939dbbae4979cb334696a760dd048bb6
rupmedhi <117652883+rupmedhi@users.noreply.github.com>
2022-12-02 10:43:44 +0530

    task5

---
+++ task/task5.js
@@ -0,0 +1,15 @@
+
+
+var list=document.getElementsByClassName("newlist-group-item");
+list[0].style.backgroundColor="cyan";
+list[0].style.color="red";
+
+
+
+var list2=document.getElementsByTagName("li");
+list2[4].style.fontFamily="Impact,Charcoal,sans-serif";
+
+
+
+list2[4].style.textDecoration="underline";
+
